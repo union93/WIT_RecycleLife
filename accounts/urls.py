16478f76c6477f88d473eac19b78a39b0b85e997
union93 <union93@konkuk.ac.kr>
2020-07-20 15:42:05 +0900

    delete_test

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.signup, name='signup'),
-    path('logout/', views.signup, name='signup'),
-]
